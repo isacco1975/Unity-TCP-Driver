b938b3910add1bd4b80693fbb8ef748d21276fc0
Isaac Garcia Peveri <isacco1975gp@gmail.com>
2025-10-21 13:37:45 +0200

    Working on Y

--- CubesController.cs
+++ CubesController.cs
@@ -1,13 +1,17 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
-using UnityEngine.Windows;
+
+//****************************************************
+//* Cubes Controller Script                         **
+//****************************************************
+//*         Manage the movements of the cubes in the *
+//*         scene from the data coming.              *
+//****************************************************
 
 public class CubesController : MonoBehaviour
 {
-
+    #region WORKING-STORAGE
     public TCP_Driver o;
     public bool isStringSent = false;
     char[] receivedChars;
@@ -28,8 +32,11 @@ public class CubesController : MonoBehaviour
     public int col = 0;
 
     List<string> rows;
+    #endregion
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    /// <summary>
+    /// Start
+    /// </summary>
     void Start()
     {
         RED = GameObject.Find("RED");
@@ -38,62 +45,31 @@ public class CubesController : MonoBehaviour
         YELLOW = GameObject.Find("YELLOW");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        o = GameObject.Find("TCP_Component").GetComponent<TCP_Driver>();
-        char[] charArray = new char[o.xBuffer.Length];
-
-        if (o.xBuffer.EndsWith("\r"))
-        {
-            Debug.Log("Incoming TCP message: " + o.xBuffer);
-            isStringSent = true;
-            receivedChars = o.xBuffer.ToCharArray();
-
-            row5 = new string(receivedChars, 0, 6);
-            row4 = new string(receivedChars, 6, 6);
-            row3 = new string(receivedChars, 12, 6);
-            row2 = new string(receivedChars, 18, 6);
-            row1 = new string(receivedChars, 24, 6);
-
-            rows = new List<string>();
-            rows.Clear();
-            rows.Add(row1);
-            rows.Add(row2);
-            rows.Add(row3);
-            rows.Add(row4);
-            rows.Add(row5);
-
-            MoveCubesToNewPositions();
-
-            if (isStringSent)
-            {
-                isStringSent = false;
-                o.xBuffer = "";
-
-                for (int idx = 0; idx < receivedChars.Length; idx++)
-                {
-                    receivedChars[idx] = '\0';
-                }
-            }
-        }
-    }
-
-    void MoveCube(char cubeToMove, float xPosition, float yPosition, float zPosition)
-    {
-        switch (cubeToMove)
-        {
-            case 'R': RED.transform.localPosition = new Vector3(xPosition, yPosition, zPosition); break;
-            case 'G': GREEN.transform.localPosition = new Vector3(xPosition, yPosition, zPosition); break;
-            case 'B': BLUE.transform.localPosition = new Vector3(xPosition, yPosition, zPosition); break;
-            case 'Y': YELLOW.transform.localPosition = new Vector3(xPosition, yPosition, zPosition); break;
-        }
-    }
-
-    void MoveCubesToNewPositions()
+    /// <summary>
+    /// Update position of each cube according to the received string
+    /// </summary>
+    void DrawInitialSituation()
     {
         float actualXPos = 0f, actualYPos = 0f, newXPos = 0f;
         int rowIndex = 0;
+
+        Debug.Log("Incoming TCP message: " + o.xBuffer);
+        isStringSent = true;
+        receivedChars = o.xBuffer.ToCharArray();
+
+        row5 = new string(receivedChars, 0, 6);
+        row4 = new string(receivedChars, 6, 6);
+        row3 = new string(receivedChars, 12, 6);
+        row2 = new string(receivedChars, 18, 6);
+        row1 = new string(receivedChars, 24, 6);
+
+        rows = new List<string>();
+        rows.Clear();
+        rows.Add(row1);
+        rows.Add(row2);
+        rows.Add(row3);
+        rows.Add(row4);
+        rows.Add(row5);
 
         foreach (string row in rows)
         {
@@ -130,6 +106,10 @@ public class CubesController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// This is called before updating each cube position
+    /// </summary>
+    /// <param name="a"></param>
     void GetActualPositions(string a)
     {
         if (a.Contains("R"))
@@ -161,6 +141,63 @@ public class CubesController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// The routine that updated the position of a specific cube
+    /// </summary>
+    /// <param name="cubeToMove"></param>
+    /// <param name="xPosition"></param>
+    /// <param name="yPosition"></param>
+    /// <param name="zPosition"></param>
+    void MoveCube(char cubeToMove, float xPosition, float yPosition, float zPosition)
+    {
+        switch (cubeToMove)
+        {
+            case 'R': RED.transform.localPosition = new Vector3(xPosition, yPosition, zPosition); break;
+            case 'G': GREEN.transform.localPosition = new Vector3(xPosition, yPosition, zPosition); break;
+            case 'B': BLUE.transform.localPosition = new Vector3(xPosition, yPosition, zPosition); break;
+            case 'Y': YELLOW.transform.localPosition = new Vector3(xPosition, yPosition, zPosition); break;
+        }
+    }
+
+    /// <summary>
+    /// Animating the single cube received from the C64
+    /// </summary>
+    void AnimateCube()
+    {
+        char cubeToMove = Convert.ToChar(o.xBuffer.Substring(0, 1));
+        float xPosition = float.Parse(o.xBuffer.Split(";")[4]);
+        MoveCube(cubeToMove, xPosition, 1.5f, 0.02453f);
+    }
+
+    /// <summary>
+    /// Update the scene every 1 frame
+    /// </summary>
+    void Update()
+    {
+        o = GameObject.Find("TCP_Component").GetComponent<TCP_Driver>();
+        char[] charArray = new char[o.xBuffer.Length];
+
+        if (o.xBuffer.EndsWith("\r"))
+        {
+            isStringSent = true;
+
+            if (o.xBuffer.Contains("GENERA"))
+                DrawInitialSituation();
+            else
+                AnimateCube();
+
+            if (isStringSent)
+            {
+                isStringSent = false;
+                o.xBuffer = "";
+
+                for (int idx = 0; idx < receivedChars.Length; idx++)
+                {
+                    receivedChars[idx] = '\0';
+                }
+            }
+        }
+    }
 
     // END CLASS
 }
